8ff8137304dd03fae03677c849e0b19bf0e94cbc
김세혁 <se6816@naver.com>
2023-09-26 15:28:44 +0900

    :sparkles: JPAAuditing 추가 #4

--- src/main/java/sieum/member/MemberServiceApplication.java
+++ src/main/java/sieum/member/MemberServiceApplication.java
@@ -1,9 +1,15 @@
 package sieum.member;
 
+import javax.persistence.EntityListeners;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EntityListeners(AuditingEntityListener.class)
+@EnableJpaAuditing
 public class MemberServiceApplication {
 
     public static void main(String[] args) {
