2a94f2377aa1fc23fc3338ec0c81e5faece46a19
김세혁 <se6816@naver.com>
2023-09-26 15:29:13 +0900

    :art: Follower createdDate 추가 #4

--- src/main/java/sieum/member/entity/Follower.java
+++ src/main/java/sieum/member/entity/Follower.java
@@ -1,11 +1,15 @@
 package sieum.member.entity;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
+
+import org.springframework.data.annotation.CreatedDate;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,5 +31,9 @@ public class Follower {
 	@MapsId("followerId")
 	@JoinColumn(name = "member_id", nullable = false)
 	private Member follower;
+
+	@CreatedDate
+	@Column(name="follow_created_date",nullable = false)
+	private LocalDateTime createdDate;
 
 }
